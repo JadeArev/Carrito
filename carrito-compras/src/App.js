6a11dda162915b801aaa61a71d2b91790ae5aab7
Jade <jade.arevort@gmail.com>
2022-10-31 08:25:43 -0300

    .

---
+++ carrito-compras/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+
+function App() {
+  <BrowserRouter>
+    <Routes>
+    </Routes>
+  </BrowserRouter>
+}
+
+export default App;
